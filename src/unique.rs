cb4dd6ac97e92713d5ee70a5a587cbd220ec958d
nevakrien <nevo.krien@gmail.com>
2025-08-13 22:24:30 +0300

    inlined the arena

--- src/unique.rs
+++ src/unique.rs
@@ -1,260 +1,276 @@
-use std::cell::UnsafeCell;
-use typed_arena::Arena;
-use std::collections::HashSet;
-use std::hash::{Hash, Hasher};
-use std::marker::PhantomData;
-use std::ops::Deref;
-use std::ptr;
 use std::slice;
-use std::str;
+use std::hash::Hash;
+use std::sync::RwLock;
+use std::collections::HashSet;
+// use std::hash::{Hash, Hasher};
+// use std::marker::PhantomData;
+// use std::ops::Deref;
+// use std::ptr;
+// use std::slice;
+// use std::str;
 
-// ========== UniqueSlice ==========
-pub struct UniqueSlice<'a, T> {
-    ptr: *const T,
-    len: usize,
-    _ph: PhantomData<&'a [T]>,
-}
+// // ========== UniqueSlice ==========
+// struct UniqueSlice<'a, T> {
+//     ptr: *const T,
+//     len: usize,
+//     _ph: PhantomData<&'a [T]>,
+// }
 
-// ---- Manual trait impls (no T bounds) ----
-impl<'a, T> Clone for UniqueSlice<'a, T> {
-    #[inline]
-    fn clone(&self) -> Self { *self }
-}
+// // ---- Manual trait impls (no T bounds) ----
+// impl<'a, T> Clone for UniqueSlice<'a, T> {
+//     #[inline]
+//     fn clone(&self) -> Self { *self }
+// }
 
-impl<'a, T> Copy for UniqueSlice<'a, T> {}
+// impl<'a, T> Copy for UniqueSlice<'a, T> {}
 
-impl<'a, T> PartialEq for UniqueSlice<'a, T> {
-    #[inline]
-    fn eq(&self, other: &Self) -> bool {
-        self.ptr == other.ptr && self.len == other.len
-    }
-}
+// impl<'a, T> PartialEq for UniqueSlice<'a, T> {
+//     #[inline]
+//     fn eq(&self, other: &Self) -> bool {
+//         self.ptr == other.ptr && self.len == other.len
+//     }
+// }
 
-impl<'a, T> Eq for UniqueSlice<'a, T> {}
+// impl<'a, T> Eq for UniqueSlice<'a, T> {}
 
-impl<'a, T> Hash for UniqueSlice<'a, T> {
-    #[inline]
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.ptr.hash(state);
-        self.len.hash(state);
-    }
-}
+// impl<'a, T> Hash for UniqueSlice<'a, T> {
+//     #[inline]
+//     fn hash<H: Hasher>(&self, state: &mut H) {
+//         self.ptr.hash(state);
+//         self.len.hash(state);
+//     }
+// }
 
-// ---- PartialEq with slices ----
-impl<'a, T: PartialEq> PartialEq<[T]> for UniqueSlice<'a, T> {
-    #[inline]
-    fn eq(&self, other: &[T]) -> bool {
-        let this: &[T] = (*self).into();
-        this == other
-    }
-}
-impl<'a, T: PartialEq> PartialEq<&'a [T]> for UniqueSlice<'a, T> {
-    #[inline]
-    fn eq(&self, other: &&'a [T]) -> bool {
-        let this: &[T] = (*self).into();
-        this == *other
-    }
-}
+// // ---- PartialEq with slices ----
+// impl<'a, T: PartialEq> PartialEq<[T]> for UniqueSlice<'a, T> {
+//     #[inline]
+//     fn eq(&self, other: &[T]) -> bool {
+//         let this: &[T] = (*self).into();
+//         this == other
+//     }
+// }
+// impl<'a, T: PartialEq> PartialEq<&'a [T]> for UniqueSlice<'a, T> {
+//     #[inline]
+//     fn eq(&self, other: &&'a [T]) -> bool {
+//         let this: &[T] = (*self).into();
+//         this == *other
+//     }
+// }
 
-// ---- Conversions ----
-impl<'a, T> From<&'a [T]> for UniqueSlice<'a, T> {
-    fn from(s: &'a [T]) -> Self {
-        let ptr = if s.is_empty() {
-            ptr::null()
-        } else {
-            s.as_ptr()
-        };
-        Self {
-            ptr,
-            len: s.len(),
-            _ph: PhantomData,
-        }
-    }
-}
+// // ---- Conversions ----
+// impl<'a, T> From<&'a [T]> for UniqueSlice<'a, T> {
+//     fn from(s: &'a [T]) -> Self {
+//         let ptr = if s.is_empty() {
+//             ptr::null()
+//         } else {
+//             s.as_ptr()
+//         };
+//         Self {
+//             ptr,
+//             len: s.len(),
+//             _ph: PhantomData,
+//         }
+//     }
+// }
 
-impl<'a, T> From<UniqueSlice<'a, T>> for &'a [T] {
-    fn from(s: UniqueSlice<'a, T>) -> Self {
-        let p = if s.ptr.is_null() {
-            ptr::NonNull::dangling().as_ptr()
-        } else {
-            s.ptr
-        };
-        unsafe { slice::from_raw_parts(p, s.len) }
-    }
-}
+// impl<'a, T> From<UniqueSlice<'a, T>> for &'a [T] {
+//     fn from(s: UniqueSlice<'a, T>) -> Self {
+//         let p = if s.ptr.is_null() {
+//             ptr::NonNull::dangling().as_ptr()
+//         } else {
+//             s.ptr
+//         };
+//         unsafe { slice::from_raw_parts(p, s.len) }
+//     }
+// }
 
-// ---- Deref ----
-impl<T> Deref for UniqueSlice<'_, T> {
-    type Target = [T];
-    fn deref(&self) -> &[T] {
-        let p = if self.ptr.is_null() {
-            ptr::NonNull::dangling().as_ptr()
-        } else {
-            self.ptr
-        };
-        unsafe { slice::from_raw_parts(p, self.len) }
-    }
-}
+// // ---- Deref ----
+// impl<T> Deref for UniqueSlice<'_, T> {
+//     type Target = [T];
+//     fn deref(&self) -> &[T] {
+//         let p = if self.ptr.is_null() {
+//             ptr::NonNull::dangling().as_ptr()
+//         } else {
+//             self.ptr
+//         };
+//         unsafe { slice::from_raw_parts(p, self.len) }
+//     }
+// }
 
-// ---- Send/Sync ----
-unsafe impl<'a, T: Sync> Send for UniqueSlice<'a, T> {}
-unsafe impl<'a, T: Sync> Sync for UniqueSlice<'a, T> {}
+// // ---- Send/Sync ----
+// unsafe impl<'a, T> Send for UniqueSlice<'a, T> where &'a T : Send {}
+// unsafe impl<'a, T> Sync for UniqueSlice<'a, T> where &'a T : Sync {}
 
 
-// ========== UniqueStr ==========
+// // ========== UniqueStr ==========
+// pub(crate) struct UniqueStr<'a>(UniqueSlice<'a, u8>);
 
-#[repr(transparent)]
-pub struct UniqueStr<'a>(UniqueSlice<'a, u8>);
+// // ---- Manual trait impls ----
+// impl<'a> Clone for UniqueStr<'a> {
+//     #[inline]
+//     fn clone(&self) -> Self { *self }
+// }
 
-// ---- Manual trait impls ----
-impl<'a> Clone for UniqueStr<'a> {
-    #[inline]
-    fn clone(&self) -> Self { *self }
-}
+// impl<'a> Copy for UniqueStr<'a> {}
 
-impl<'a> Copy for UniqueStr<'a> {}
+// impl<'a> PartialEq for UniqueStr<'a> {
+//     #[inline]
+//     fn eq(&self, other: &Self) -> bool {
+//         self.0 == other.0
+//     }
+// }
 
-impl<'a> PartialEq for UniqueStr<'a> {
-    #[inline]
-    fn eq(&self, other: &Self) -> bool {
-        self.0 == other.0
-    }
-}
+// impl<'a> Eq for UniqueStr<'a> {}
 
-impl<'a> Eq for UniqueStr<'a> {}
+// impl<'a> Hash for UniqueStr<'a> {
+//     #[inline]
+//     fn hash<H: Hasher>(&self, state: &mut H) {
+//         self.0.hash(state)
+//     }
+// }
 
-impl<'a> Hash for UniqueStr<'a> {
-    #[inline]
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.0.hash(state)
-    }
-}
+// // ---- PartialEq with str ----
+// impl<'a> PartialEq<str> for UniqueStr<'a> {
+//     #[inline]
+//     fn eq(&self, other: &str) -> bool {
+//         let this: &str = (*self).into();
+//         this == other
+//     }
+// }
+// impl<'a> PartialEq<&'a str> for UniqueStr<'a> {
+//     #[inline]
+//     fn eq(&self, other: &&'a str) -> bool {
+//         let this: &str = (*self).into();
+//         this == *other
+//     }
+// }
 
-// ---- PartialEq with str ----
-impl<'a> PartialEq<str> for UniqueStr<'a> {
-    #[inline]
-    fn eq(&self, other: &str) -> bool {
-        let this: &str = (*self).into();
-        this == other
-    }
-}
-impl<'a> PartialEq<&'a str> for UniqueStr<'a> {
-    #[inline]
-    fn eq(&self, other: &&'a str) -> bool {
-        let this: &str = (*self).into();
-        this == *other
-    }
-}
+// // ---- Conversions ----
+// impl<'a> From<&'a str> for UniqueStr<'a> {
+//     fn from(s: &'a str) -> Self {
+//         UniqueStr(UniqueSlice::from(s.as_bytes()))
+//     }
+// }
 
-// ---- Conversions ----
-impl<'a> From<&'a str> for UniqueStr<'a> {
-    fn from(s: &'a str) -> Self {
-        UniqueStr(UniqueSlice::from(s.as_bytes()))
-    }
-}
+// impl<'a> From<UniqueStr<'a>> for &'a str {
+//     fn from(us: UniqueStr<'a>) -> Self {
+//         let s: &'a [u8] = us.0.into();
+//         unsafe { str::from_utf8_unchecked(s) }
+//     }
+// }
 
-impl<'a> From<UniqueStr<'a>> for &'a str {
-    fn from(us: UniqueStr<'a>) -> Self {
-        let s: &'a [u8] = us.0.into();
-        unsafe { str::from_utf8_unchecked(s) }
-    }
-}
-
-// ---- Deref ----
-impl Deref for UniqueStr<'_> {
-    type Target = str;
-    fn deref(&self) -> &str {
-        let s: &[u8] = self.0.deref();
-        unsafe { str::from_utf8_unchecked(s) }
-    }
-}
+// // ---- Deref ----
+// impl Deref for UniqueStr<'_> {
+//     type Target = str;
+//     fn deref(&self) -> &str {
+//         let s: &[u8] = self.0.deref();
+//         unsafe { str::from_utf8_unchecked(s) }
+//     }
+// }
 
 // ==== registery
-pub struct Registery<'a, T> {
-    arena: UnsafeCell<Arena<T>>,
+struct RegisteryInner<'a, T> {
+	//we inline the arena implementation here
+	//this is mostly for not needing to mess with sync/send
+    arena_cur:Vec<T>,
+    arena_store:  Vec<Vec<T>>,
     exists: HashSet<&'a [T]>,
 }
 
 
-impl<'a,T: Clone + Eq + Hash> Registery<'a, T> {
+impl<'a, T> RegisteryInner<'a, T> {
     pub fn new() -> Self {
         Self {
-            arena: UnsafeCell::new(Arena::new()),
+            arena_cur:Vec::with_capacity(Self::get_base_size()),
+    		arena_store:Vec::new(),
             exists: HashSet::new(),
         }
     }
 
-    pub fn get(&mut self, s: &[T]) -> UniqueSlice<'a, T> {
-        if let Some(ans) = self.exists.get(s) {
-            return (*ans).into();
-        }
-        // Safety: &mut self ensures exclusive access to arena
-        let arena = unsafe { &mut *self.arena.get() };
-        let ans: &'a [T] = arena.alloc_extend(s.iter().cloned());
-        let uniq = ans.into();
-        self.exists.insert(ans);
-        uniq
+    const fn get_base_size() -> usize{
+    	let a = 4096/size_of::<T>();
+    	if a > 8 {
+    		a
+    	}else{
+    		8
+    	}
+    }
+
+    const fn get_small_size() -> usize{
+    	let a = 4096/size_of::<T>();
+    	if a > 4 {
+    		a
+    	}else{
+    		4
+    	}
+    }
+
+    unsafe fn alloc(&mut self,s:&[T])->&'a [T] where T:Clone{
+    	let start = self.arena_cur.len();
+    	if self.arena_cur.capacity()-start <= s.len(){
+    		self.arena_cur.extend(s.iter().cloned());
+    		unsafe{
+    			return slice::from_raw_parts(self.arena_cur.as_ptr().add(start),s.len());
+    		}
+    	}
+
+    	//store the too full current in
+    	{
+    		let mut new_cur = Vec::with_capacity(Self::get_base_size());
+    		std::mem::swap(&mut self.arena_cur,&mut new_cur);
+    		self.arena_store.push(new_cur);
+    	}
+
+    	//big enough to exist with another allocation
+    	if s.len() <= Self::get_small_size() {
+    		self.arena_cur.extend(s.iter().cloned());
+    		unsafe{
+    			return slice::from_raw_parts(self.arena_cur.as_ptr().add(start),s.len());
+    		}
+    	}
+
+    	//too big to do anything meaningful with just make ita seprate allocation
+    	let single_use : Vec<T> = s.into();
+    	let ans = unsafe{
+    		slice::from_raw_parts(single_use.as_ptr(),s.len())
+    	};
+    	self.arena_store.push(single_use);
+    	ans
+
     }
 }
 
-// ======== StrRegistery ========
 
-pub struct StrRegistery<'a> {
-    arena: UnsafeCell<Arena<u8>>,
-    exists: HashSet<&'a str>,
+pub struct Registery<'a, T>(RwLock<RegisteryInner<'a,T>>);
+impl<T> Registery<'_,T>{
+	pub fn new()->Self{Self(RwLock::new(RegisteryInner::new()))}
+
 }
 
-
-impl<'a> StrRegistery<'a> {
-    pub fn new() -> Self {
-        Self {
-            arena: UnsafeCell::new(Arena::new()),
-            exists: HashSet::new(),
-        }
-    }
-
-    pub fn get(&mut self, s: &str) -> UniqueStr<'a> {
-        if let Some(ans) = self.exists.get(s) {
-            return (*ans).into();
-        }
-        // Safety: &mut self ensures exclusive access to arena
-        let arena = unsafe { &mut *self.arena.get() };
-        let bytes = arena.alloc_extend(s.as_bytes().iter().copied());
-        let arena_str = unsafe { std::str::from_utf8_unchecked(bytes) };
-        let uniq = UniqueStr::from(arena_str);
-        self.exists.insert(arena_str);
-        uniq
-    }
+impl<'a,T:Hash+Eq> Registery<'a,T>{
+	pub fn try_get(&'a self,s:&[T])->Option<&'a [T]>{
+		self.0.read().unwrap().exists.get(s).map(|v| &**v)
+	}
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::sync::{Arc, Mutex};
-    use std::thread;
-    
+impl<'a,T:Hash+Eq+Clone> Registery<'a,T>{
+	pub fn get(&'a self,s:&[T])->&'a [T]{
+		//first try the fast path no relocking
+		if let Some(ans) = self.try_get(s) {
+			return ans;
+		}
 
-    #[test]
-    fn concurrent_str_registery_usage() {
-        let registry = Arc::new(Mutex::new(StrRegistery::new()));
+		self.get_write(s)
+	}
 
-        let s1 = "hello";
-        let s2 = "world";
+	pub fn get_write(&'a self,s:&[T])->&'a [T]{
+		let mut p = self.0.write().unwrap();
+		if let Some(ans) = p.exists.get(s).map(|v| &**v){
+			return ans;
+		}
 
-        let mut handles = Vec::new();
-
-        for _ in 0..2 {
-            let registry = Arc::clone(&registry);
-            handles.push(thread::spawn(move || {
-                for _ in 0..100 {
-                    let _u1 = registry.lock().unwrap().get(s1);
-                    let _u2 = registry.lock().unwrap().get(s2);
-                }
-            }));
-        }
-
-        for h in handles {
-            h.join().unwrap();
-        }
-    }
+		let ans = unsafe{p.alloc(s)};
+		p.exists.insert(ans);
+		ans
+	}
 }
